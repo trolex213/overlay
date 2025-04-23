a4c0371ac4d6c9ee90d0d894f5ea5308a1333d92
Troy Zhang <troyz0213@gmail.com>
2025-04-23 17:07:37 -0400

    Initial Commit

---
+++ ScreenOverlay/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  ScreenOverlay
+//
+//  Created by Tony Zhang on 4/23/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ ScreenOverlay/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ScreenOverlay
+//
+//  Created by Tony Zhang on 4/23/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
